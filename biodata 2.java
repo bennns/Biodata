5d48970e3834033ec1b15b9886a6b43a454ebbed
bennns <albanisayidina05@gmail.com>
2026-04-11 15:26:39 +0700

    Add files via upload

---
+++ biodata 2.java
@@ -0,0 +1,8 @@
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("Nama : Albani Sayidina");
+        System.out.println("Asal : Klaten");
+        System.out.println("Umur : 29");
+        System.out.println("Hobi : -");
+    }
+}
